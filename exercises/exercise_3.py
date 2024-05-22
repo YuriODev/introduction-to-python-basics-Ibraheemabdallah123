3eac763b32923300e7ce0eeff3f25d25f4cfae19
Ibraheemabdallah123 <ibraheemabdallah1313@gmail.com>
2024-05-22 14:07:10 +0000

    upload

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -4,4 +4,5 @@ seconds = int(input("enter a number of seconds"))
 hours = seconds // 3600
 minutes = (seconds % 3600) // 60
 seconds = ((seconds % 3600) % 60 ) % 60
-print(str(hours) + ":" + str(minutes) + ":" + str(seconds))
+# print(hours + ":" + minutes + ":" + seconds)
+print(f"{hours}:{minutes:02d}:{seconds:02d}")
